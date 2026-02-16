52eda26996eecba66e46a633a5b5d71b2ced04cb
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-16 01:20:58 -0300

    add

---
+++ abstract-factory/abstract-factory/Logic/PaymentService.cs
@@ -0,0 +1,26 @@
+﻿using abstract_factory.Interface;
+
+namespace abstract_factory.Logic;
+
+internal class PaymentService(IPaymentGatewayFactory factory)
+{
+    private readonly IPaymentGatewayFactory _factory = factory;
+
+    public void ProcessPayment(string cardNumber, decimal amount)
+    {
+        var validator = _factory.CreateValidator();
+
+        if (!validator.ValidateCard(cardNumber))
+        {
+            Console.WriteLine("Invalid");
+            return;
+        }
+
+        var processor = _factory.CreateProcessor();
+        var result = processor.ProcessTransaction(amount, cardNumber);
+
+        var logger = _factory.CreateLogger();
+
+        logger.Log($"Transação processada: {result}");
+    }
+}
