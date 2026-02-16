964c05ddf7356335b4e1c6bb12b8afd96c9af5cf
BrunoSFreschi <bruno.freschi2@gmail.com>
2026-02-16 01:19:59 -0300

    add

---
+++ abstract-factory/abstract-factory/Program.cs
@@ -0,0 +1,13 @@
+﻿using abstract_factory.Logic;
+using abstract_factory.Logic.MercadoPago;
+using abstract_factory.Logic.PagSeguro;
+
+var pagSeguroService = new PaymentService(new PagSeguroFactory());
+pagSeguroService.ProcessPayment("1234567890123456", 150);
+
+Console.WriteLine();
+
+var mercadoPagoService = new PaymentService(new MercadoPagoFactory());
+mercadoPagoService.ProcessPayment("5234567890123456", 200);
+
+Console.WriteLine();
